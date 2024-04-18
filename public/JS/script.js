e997a46cfc0212a42165cc995ec5e58b56b64857
Mr.Krabs <>
2024-04-18 16:09:34 +0300

    --JAVASCRIPT VALIDATION

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,22 +1,26 @@
 let child, teen, adult;
-let theValue;
 
 function calculate_price()
 {
-    child= document.getElementById('child').value;
-    teen= document.getElementById('teen').value;
-    adult= document.getElementById('adult').value;
+    child= is_number_allowed(document.getElementById('child').value);
+    document.getElementById('child').value = child;
+    teen= is_number_allowed(document.getElementById('teen').value);
+    document.getElementById('teen').value = teen;
+    adult= is_number_allowed(document.getElementById('adult').value);
+    document.getElementById('adult').value = adult;
 
     document.getElementById("f_price").innerHTML= Number(child)*15+Number(teen)*20+Number(adult)*30 + " €"
 }
 
-calculate_price();
-document.getElementById('child').addEventListener('input', function(){
-    calculate_price();
-});
-document.getElementById('teen').addEventListener('input', function(){
-    calculate_price();
-});
-document.getElementById('adult').addEventListener('input', function(){
+function is_number_allowed(x)
+{
+    if (x<0){return 0;}
+    else return x;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('child').addEventListener('input', calculate_price);
+    document.getElementById('teen').addEventListener('input', calculate_price);
+    document.getElementById('adult').addEventListener('input', calculate_price);
     calculate_price();
 });
